d307bb8d9ef221f7547cfa21018f97d74212931a
Kylian Courel <kyliancourel@Kylians-MacBook-Pro.local>
2025-07-24 23:48:05 +0200

    Changement de firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+// src/firebase.js
+
 // Import des fonctions Firebase nécessaires
 import { initializeApp } from "firebase/app";
 import {
@@ -12,17 +14,17 @@ import {
   signInWithPopup,
 } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";  // <-- import storage
+import { getStorage } from "firebase/storage";
 
 // Configuration Firebase (clé publique)
 const firebaseConfig = {
   apiKey: "AIzaSyBn_zM7FTF6BmwEsG_zR6SWpD1Edkj--hQ",
   authDomain: "juego-espanol-54a58.firebaseapp.com",
   projectId: "juego-espanol-54a58",
-  storageBucket: "juego-espanol-54a58.appspot.com",  // <-- corrigé storageBucket (ajout .appspot.com)
+  storageBucket: "juego-espanol-54a58.appspot.com",
   messagingSenderId: "946808641255",
   appId: "1:946808641255:web:403e28b302d096928c3442",
-  measurementId: "G-4E08JJ9Z96"
+  measurementId: "G-4E08JJ9Z96",
 };
 
 // Initialisation de Firebase
@@ -31,13 +33,13 @@ const app = initializeApp(firebaseConfig);
 // Initialisation Auth, Firestore et Storage
 const auth = getAuth(app);
 const db = getFirestore(app);
-const storage = getStorage(app);  // <-- ajout stockage
+const storage = getStorage(app);
 
 // Export des fonctions et objets utiles
 export {
   auth,
   db,
-  storage,  // <-- export storage
+  storage,
   signInWithEmailAndPassword,
   createUserWithEmailAndPassword,
   signOut,
