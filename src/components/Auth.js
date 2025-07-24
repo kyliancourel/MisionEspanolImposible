876a3e8890b3433b00ce7cf7c0f8b839a3dcd2db
Kylian Courel <kyliancourel@Kylians-MacBook-Pro.local>
2025-07-24 23:55:01 +0200

    Modification de Auth.js

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import { auth, db } from './firebase';
+import { auth, db } from '../firebase';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
