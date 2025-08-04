94afd074c2f24c866abdd2035cfa70ff861d372e
Kylian Courel <kyliancourel@Kylians-MacBook-Pro.local>
2025-08-04 23:57:09 +0200

    New modification encrypted-key.js

--- libs/encrypted-key.js
+++ libs/encrypted-key.js
@@ -1,2 +1,2 @@
 // Clé AES chiffrée (issue de ta console)
-export const encryptedAESKey = "U2FsdGVkX19otrYb3LexhvYkJ25znUsGMt2rVm3Lop1qZMKi8yYtp+yS+ZeLAeS4YQWmR89AHLiKux1Nek3qNw==";
+export const secretKey = "U2FsdGVkX19otrYb3LexhvYkJ25znUsGMt2rVm3Lop1qZMKi8yYtp+yS+ZeLAeS4YQWmR89AHLiKux1Nek3qNw==";
