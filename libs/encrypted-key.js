a257eeac648455e9da9c43e65aa473ce607c7224
Kylian Courel <kyliancourel@Kylians-MacBook-Pro.local>
2025-08-04 23:53:54 +0200

    Modification validate.html & encrypted-key.js

--- libs/encrypted-key.js
+++ libs/encrypted-key.js
@@ -1,4 +1,2 @@
 // Clé AES chiffrée (issue de ta console)
-const encryptedAESKey = "U2FsdGVkX19otrYb3LexhvYkJ25znUsGMt2rVm3Lop1qZMKi8yYtp+yS+ZeLAeS4YQWmR89AHLiKux1Nek3qNw==";
-
-export default encryptedAESKey;
+export const encryptedAESKey = "U2FsdGVkX19otrYb3LexhvYkJ25znUsGMt2rVm3Lop1qZMKi8yYtp+yS+ZeLAeS4YQWmR89AHLiKux1Nek3qNw==";
